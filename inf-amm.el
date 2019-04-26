90332676429127c14a467a2082491285d5fb50cf
bobrowadam <adam@bigpanda.io>
2019-04-26 14:06:12 +0300

    Initial commit

---
+++ inf-amm.el
@@ -0,0 +1,35 @@
+;;; name.el --- summary -*- lexical-binding: t -*-
+
+;; Author: adam bob
+;; Maintainer: adam bob
+;; Version: 1.0
+;; Package-Requires: dash
+;; Homepage: TBD
+;; Keywords: ammonite, scala
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary: 
+
+;; commentary
+;; inf-ammonite is a simple mode for running "Ammonite" insdie Emacs using an inferior process.
+;;; Code:
+
+(provide 'inf-amm)
+
+;;; name.el ends here
